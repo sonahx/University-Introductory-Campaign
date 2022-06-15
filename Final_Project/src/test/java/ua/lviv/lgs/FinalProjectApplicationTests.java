130c739fe7b0878c25c46cd643c5829f66a6b370
Taras Peles <pelestaras@gmail.com>
2022-06-15 22:33:33 +0300

    Initial project commit

---
+++ Final_Project/src/test/java/ua/lviv/lgs/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
